a8caf0c2268a9a2f5e1a3e22fd8f85e6c248373f
mauroson <mcontaalternativa@gmail.com>
2023-11-26 16:03:02 -0300

    Revert "Revert "teste""
    This reverts commit 0807c31210424cf5c85895c90067a3c6a457741d.

--- sonho-gourmet-next/src/components/navbar.tsx
+++ sonho-gourmet-next/src/components/navbar.tsx
@@ -3,20 +3,20 @@ import styles from "@/styles/Navbar.module.css"
 export default function NavBar(){
     return(
         <nav id={styles.nav}>
-            <a href="" id={styles.logo}>
+            <a href="/" id={styles.logo}>
             <div id={styles.logo}>
-                <img src='LogoIMG.png'></img>
+                <img src='/LogoIMG.png'></img>
             </div>
             </a>
             <div id={styles.mainNav}>
-                <a href="" className={styles.linkNav}><div className={styles.mainNavButton}>Cardápio</div></a>
-                <a href="" className={styles.linkNav}><div className={styles.mainNavButton}>Sobre</div></a>
-                <a href="" className={styles.linkNav}><div className={styles.mainNavButton}>Contatos</div></a>
+                <a href="/Cardapio" className={styles.linkNav}><div className={styles.mainNavButton}>Cardápio</div></a>
+                <a href="/Sobre" className={styles.linkNav}><div className={styles.mainNavButton}>Sobre</div></a>
+                <a href="/Contato" className={styles.linkNav}><div className={styles.mainNavButton}>Contatos</div></a>
             </div>
-            <a href="" className={styles.linkNav}>
+            <a href="/Login" className={styles.linkNav}>
                 <div id={styles.login}>
                     <div>Login</div>
-                    <div><img src='UserIcon.png' height='80px'></img></div>
+                    <div><img src='/UserIcon.png' height='80px'></img></div>
                 </div>
             </a> 
         </nav>
