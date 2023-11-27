3ed6b6dcdd75d59aadaa7828ad06f05a01b40dc5
mauroson <mcontaalternativa@gmail.com>
2023-11-26 22:18:33 -0300

    Adding LoginPage

--- sonho-gourmet-next/src/pages/Login/index.tsx
+++ sonho-gourmet-next/src/pages/Login/index.tsx
@@ -1,5 +1,26 @@
+import styles from '@/styles/Login.module.css'
 export default function Login(){
     return(
-        <h1>Login</h1>
+        <div id={styles.screen}>
+            <div id={styles.formContainer}>
+                <div id={styles.form}>
+                    <div id={styles.back}>
+                        <img src='backButton.png'></img>
+                    </div>
+                    <div id={styles.form2}>
+                        <form id={styles.form3}>
+                            <h1 className={styles.cell} id={styles.title}>Faça login na plataforma</h1>
+                            <label className={styles.cell}>E-mail:</label>
+                            <input></input>
+                            <label className={styles.cell}>Senha:</label>
+                            <input></input>
+                            <button>Enviar</button>
+                        </form>  
+                    </div>
+                </div>      
+            </div>
+            <div id={styles.img}>
+            </div>
+        </div>
     )
 }
