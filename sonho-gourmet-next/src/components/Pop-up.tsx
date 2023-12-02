331be67f1d44028b086834859027e78d6681d9eb
weldoj <weldoj201008@gmail.com>
2023-12-02 05:00:58 -0300

    popup_finalizado

--- sonho-gourmet-next/src/components/Pop-up.tsx
+++ sonho-gourmet-next/src/components/Pop-up.tsx
@@ -7,30 +7,34 @@ export default function PopUp(){
     return(
         <>
         {close && (
-            <dialog className={styles.section}>
-            <div>
-                <a href="#"><img src="/AddImageButton.png" alt="" /></a>
-            </div>
-            <div>
-                <label htmlFor="inome">Nome</label>
-                <input type="text" />
-            </div>
-            <div>
-                <label htmlFor="idescricao">Descrição</label>
-                <input type="text" />
-            </div>
-            <div>
-                <div>
-                    <label htmlFor="ipreco">Preço</label>
-                    <input type="number" />
+            <>
+            <div className={styles.overlay} onClick={vaifechar}></div>
+            <section className={styles.section}>
+                <button className={styles.btnSair} onClick={vaifechar}><img src="/x_icon_150997.png" alt="" /></button>
+                <div className={styles.addImg}>
+                    <a href="#"><img src="/AddImageButton.png" alt="" /></a>
                 </div>
-                <div>
-                    <label htmlFor="icategoria">Categoria</label>
+                <div className={styles.nome}>
+                    <label htmlFor="inome">Nome</label>
                     <input type="text" />
                 </div>
-            </div>
-            <button onClick={vaifechar}>Salvar</button>
-        </dialog>
+                <div className={styles.descricaoDiv}>
+                    <label htmlFor="idescricao">Descrição</label>
+                    <textarea name="idescricao" id={styles.descricao}></textarea>
+                </div>
+                <div className={styles.preco_categoria}>
+                    <div className={styles.preco}>
+                        <label htmlFor="ipreco">Preço</label>
+                        <input type="number" />
+                    </div>
+                    <div className={styles.categoria}>
+                        <label htmlFor="icategoria">Categoria</label>
+                        <input type="text" />
+                    </div>
+                </div>
+                <button className={styles.btn} onClick={vaifechar}>Salvar</button>
+        </section>
+        </>
         )}
         </>
     );
